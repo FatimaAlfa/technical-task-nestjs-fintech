0edf2cd736cbb399e1c1a3f43e2e2aa6dff16e71
FatimaAl <36174974+FatimaAl@users.noreply.github.com>
2025-11-08 14:08:29 +0200

    Update dependencies and enhance application security features
    - Upgraded NestJS packages to version 11.1.8 and added new dependencies: @nestjs/passport, @nestjs/swagger, class-transformer, class-validator, express, express-basic-auth, and helmet.
    - Removed the app.controller.spec.ts test file.
    - Implemented JWT authentication guard in AppModule.
    - Enhanced main.ts with global exception handling, validation pipes, basic authentication, Swagger documentation setup, CORS configuration, and middleware for JSON and URL-encoded request handling.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './common/guards/auth.guard';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,65 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
+import basicAuth from 'express-basic-auth';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const authOptions = {
+    challenge: true,
+    users: {
+      fatima: 'fatima123',
+    },
+    authorizeAsync: false,
+    unauthorizedResponse: 'Unauthorized',
+  };
+  app.use('/api', basicAuth(authOptions));
+
+  const config = new DocumentBuilder()
+    .setTitle('TECHNICAL TASK API')
+    .setDescription(
+      'simplified Fintech Payment Gateway API using NestJS and MongoDB, with a focus on secure data handling (PCI DSS awareness) and audit logging for critical actions.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const whitelist = ['http://localhost:3050', 'http://127.0.0.1:3050'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, origin);
+      } else callback(new Error('Not allowed by CORS'));
+    },
+
+    credentials: true,
+  });
+  app.use(helmet());
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  await app.listen(3050);
 }
 bootstrap();
